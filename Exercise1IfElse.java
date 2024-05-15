dc46133b988dbce321af449ec71e6bc77e798677
Marbs <Ml09278984182@gmail.com>
2024-05-15 20:27:34 +0800

    First commit

---
+++ Exercise1IfElse.java
@@ -0,0 +1,13 @@
+public class Exercise1IfElse {
+    public static void main(String[] args) {
+        int count = 9;
+
+        if (count > 10) {
+            System.out.println("Count is Greater than 10");
+        }
+
+        else {
+            System.out.println("Count is less than 10");
+        }
+    }
+}
